149ac4f338d1ca613ce5d77918bcbb35f5099138
Abhishek <Abhishek@DESKTOP-OI8UEJV>
2025-04-14 22:05:35 +0530

    Add project files.

---
+++ DropDownListAspCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DropDownListAspCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
